51832b7b56057874c34d0f4c122f85a787a83ee6
Kirill Vainer <shadowislord@gmail.com>
2016-11-27 15:54:25 -0500

    upload_native: check

--- private/upload_native.sh
+++ private/upload_native.sh
@@ -6,7 +6,7 @@ set -euo pipefail
     git config --global user.name "travis-ci"
     # openssl aes-256-cbc -K $encrypted_f0a0b284e2e8_key -iv $encrypted_f0a0b284e2e8_iv -in private/key.enc -out "$HOME/.ssh/id_rsa" -d
     # chmod 600 "$HOME/.ssh/id_rsa"
-    md5sum jme3-bullet-native/libs/native/osx/x86/libbulletjme.dylib jme3-bullet-native/build/libs/bulletjme/shared/mac32/libbulletjme.so
+    md5 -r jme3-bullet-native/libs/native/osx/x86/libbulletjme.dylib jme3-bullet-native/build/libs/bulletjme/shared/mac32/libbulletjme.so
     git checkout "$TRAVIS_BRANCH"
     git add -v -- jme3-bullet-native/libs/native/
     git commit -v -m "[ci skip] bullet: update $TRAVIS_OS_NAME natives"
